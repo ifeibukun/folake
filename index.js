029dc41ce49bf11b6a81e5cfcfdd06c70075c74e
ifeibukun <138507467+ifeibukun@users.noreply.github.com>
2023-09-26 12:24:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+function hey() {
+   var show = document.getElementById("second-container");
+   show.style.display = "block"
+   var hide = document.getElementById("whole-container");
+   hide.style.display = "none"
+   var showsecond = document.getElementById("none-second");
+    showsecond.style.display = "block"
+    var scroll = document.getElementById("scroll")
+    scroll.style.marginTop = "60%"
+
+}
+function heyschool(){
+    var showthird = document.getElementById("whole-container");
+    showthird.style.display = "block"
+
+    var showsecond = document.getElementById("second-container");
+    showsecond.style.display = "none"
+    var scroll = document.getElementById("scroll")
+    scroll.style.marginTop = "-50%"
+    
+    
+}
